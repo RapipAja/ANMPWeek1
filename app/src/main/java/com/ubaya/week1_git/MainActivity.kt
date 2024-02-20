35b8263a5ce6f5273d138440637b1b30d4dcf8b3
RapipAja <rafifprasetyo007@gmail.com>
2024-02-20 20:08:05 +0700

    Main Activity

---
+++ app/src/main/java/com/ubaya/week1_git/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.week1_git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
