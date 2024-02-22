be20d7bb503b7e9f5d525256a6213d8318edbf53
raransyw <jeonnkook12@gmail.com>
2024-02-22 10:28:43 +0700

    tambah register ya

---
+++ app/src/main/java/com/ubaya/week1_git/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.week1_git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
